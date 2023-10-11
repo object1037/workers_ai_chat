b8a5aa417f12a3158b9854956a9e6bd4dc250eaa
object1037 <contact@object1037.dev>
2023-10-11 22:47:46 +0900

    add cloudflare/ai

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,42 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { Ai } from '@cloudflare/ai'
+import { json } from '@remix-run/cloudflare'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: 'Workers AI chat' },
+    { name: 'description', content: 'AI chat app' },
+  ]
+}
+
+export interface Env {
+  AI: any
+}
+
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  const env = context.env as Env
+  const ai = new Ai(env.AI)
+
+  const systemPrompt = 'Answer within 100 words.'
+  const question = 'How to make a chat app?'
+
+  const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages: [
+      { role: 'system', content: systemPrompt },
+      { role: 'user', content: question },
+    ],
+  })
+
+  return json({ response })
+}
 
 export default function Index() {
+  const { response } = useLoaderData<typeof loader>()
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <p>{response}</p>
     </div>
-  );
+  )
 }
