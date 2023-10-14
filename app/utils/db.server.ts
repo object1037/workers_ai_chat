be216add7f0d4fc6649bc7e778c55268ed715a34
object1037 <contact@object1037.dev>
2023-10-14 23:09:44 +0900

    update sql

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,11 +1,15 @@
+import { eq } from 'drizzle-orm'
 import { drizzle } from 'drizzle-orm/d1'
-import { message } from '~/schema'
+import { message, chat } from '~/schema'
 
 export type InsertMessage = typeof message.$inferInsert
 
-export const getMessages = async (db_binding: D1Database) => {
+export const getMessages = async (db_binding: D1Database, chatId: number) => {
   const db = drizzle(db_binding)
-  const result = await db.select().from(message)
+  const result = await db
+    .select()
+    .from(message)
+    .where(eq(message.chatId, chatId))
   return result
 }
 
@@ -18,8 +22,14 @@ export const addMessage = async (
   return result
 }
 
-export const resetChat = async (db_binding: D1Database) => {
+export const resetChat = async (db_binding: D1Database, chatId: number) => {
   const db = drizzle(db_binding)
-  const result = await db.delete(message)
+  const result = await db.delete(message).where(eq(message.chatId, chatId))
+  return result
+}
+
+export const addChat = async (db_binding: D1Database, name: string) => {
+  const db = drizzle(db_binding)
+  const result = await db.insert(chat).values({ name })
   return result
 }
